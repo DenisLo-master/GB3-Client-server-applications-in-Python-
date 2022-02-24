7ea3c79f25f00ff470789c6ca1ab7914cb12a5d5
Denis <denis.lkg@gmial.com>
2022-02-24 21:24:26 +0300

    task5

--- HW1/Task5.py
+++ HW1/Task5.py
@@ -1,1 +1,17 @@
-# 5. Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать результаты из байтовового в строковый тип на кириллице.
+# 5. Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать результаты из байтовового в строковый тип на
+# кириллице.
+
+import subprocess
+import platform
+
+param = '-n' if platform.system().lower() == 'windows' else '-c'
+host_list = ['yandex.ru', 'youtube.com']
+result = {}
+
+for host_item in host_list:
+    args = ['ping', param, '2', host_item]
+    data_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+    result[host_item] = []
+    for line in data_ping:
+        result[host_item].append(line.decode("utf-8"))
+    print(*result[host_item],'-'*20)
