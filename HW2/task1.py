c2ab229e521bbf0bdb209d4b3ecc84e80ba74431
Denis <denis.lkg@gmial.com>
2022-03-01 01:10:32 +0300

    task1

--- HW2/task1.py
+++ HW2/task1.py
@@ -16,24 +16,23 @@ import re
 import numpy as np
 import csv
 
-os_prod_list=[]
-os_name_list=[]
-os_code_list=[]
-os_type_list=[]
+os_prod_list = []
+os_name_list = []
+os_code_list = []
+os_type_list = []
 
-
-parameter_os_list=['Изготовитель системы:', 'Название ОС:', 'Код продукта:', 'Тип системы:']
+parameter_os_list = ['Изготовитель системы:', 'Название ОС:', 'Код продукта:', 'Тип системы:']
 file_list = ['info_1.txt', 'info_2.txt', 'info_3.txt']
 
+
 def get_data():
+    # Вариант 1 (пункт ДЗ, фиксированный список параметров)
+    #     main_data =[]
+    ##########
 
-# Вариант 1 (пункт ДЗ, фиксированный список параметров)
-#     main_data =[]
-##########
-
-# Вариант 2  (зависиит от исходного списка параметров в переменной parameter_os_list)
-    main_data = [[" " for m in range(len(file_list))] for n in range(len(parameter_os_list))]
-##########
+    # Вариант 2  (зависиит от исходного списка параметров в переменной parameter_os_list)
+    main_data = [[" " for _ in range(len(file_list))] for _ in range(len(parameter_os_list))]
+    ##########
 
     for i in range(len(file_list)):
         with open(file_list[i], 'rb') as f:
@@ -43,43 +42,40 @@ def get_data():
         with open(file_list[i], encoding=encoding) as f:
             file_content = f.read()
 
+        # Вариант 1 (пункт ДЗ, фиксированный список параметров)
+        #     match_prod= re.search('Изготовитель\sсистемы:\s*(.*\S)(\s*\n|\n)', file_content)
+        #     os_prod_list.append(match_prod[1])
+        #     match_name = re.search('Название\sОС:\s*(.*\S)(\s*\n|\n)', file_content)
+        #     os_name_list.append(match_name[1])
+        #     match_code = re.search('Код\sпродукта:\s*(.*\S)(\s*\n|\n)', file_content)
+        #     os_code_list.append(match_code[1])
+        #     match_type = re.search('Тип\sсистемы:\s*(.*\S)(\s*\n|\n)', file_content)
+        #     os_type_list.append(match_type[1])
+        # main_data = [os_prod_list, os_name_list, os_code_list, os_type_list]
+        ##########
 
-#Вариант 1 (пункт ДЗ, фиксированный список параметров)
-    #     match_prod= re.search('Изготовитель\sсистемы:\s*(.*\S)(\s*\n|\n)', file_content)
-    #     os_prod_list.append(match_prod[1])
-    #     match_name = re.search('Название\sОС:\s*(.*\S)(\s*\n|\n)', file_content)
-    #     os_name_list.append(match_name[1])
-    #     match_code = re.search('Код\sпродукта:\s*(.*\S)(\s*\n|\n)', file_content)
-    #     os_code_list.append(match_code[1])
-    #     match_type = re.search('Тип\sсистемы:\s*(.*\S)(\s*\n|\n)', file_content)
-    #     os_type_list.append(match_type[1])
-    # main_data = [os_prod_list, os_name_list, os_code_list, os_type_list]
-##########
-
-# Вариант 2 (зависиит от исходного списка параметров в переменной parameter_os_list)
+        # Вариант 2 (зависиит от исходного списка параметров в переменной parameter_os_list)
         for j in range(len(parameter_os_list)):
-            match_regexp= f"r\'{parameter_os_list[j]}\\s*(.*\S)(\s*\\n|\\n)\'"
-            match_regexp=eval(match_regexp)
+            match_regexp = f"r\'{parameter_os_list[j]}\\s*(.*\S)(\s*\\n|\\n)\'"
+            match_regexp = eval(match_regexp)
             match = re.search(match_regexp, file_content)
-            main_data[j][i]=match[1]
+            main_data[j][i] = match[1]
 
-##########
+    ##########
 
     main_data = np.array(main_data)
     main_data = np.transpose(main_data)
     main_data = np.row_stack((parameter_os_list, main_data))
-    return main_data.tolist()
+    return main_data
 
 
 def write_to_csv(file_link):
-    with open(file_link, 'w',encoding='utf-8') as result_file:
+    with open(file_link, 'w', encoding='utf-8') as result_file:
         file_writer = csv.writer(result_file)
-        data=get_data()
-        for row in data:
-            file_writer.writerow(row)
-
-
-
+        data = get_data()
+        for data_row in data:
+            file_writer.writerow(data_row)
+    return
 
 
 write_to_csv('result_data.csv')
