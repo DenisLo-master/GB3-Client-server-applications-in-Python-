c87570c1887ba2ba1ceb327762522e9ec0df2338
Denis <Denis.lkg@gmail.com>
2022-03-10 21:24:47 +0300

    hw4 unit-test inprogress

---
+++ HW3/unit_tests/test_server.py
@@ -0,0 +1,16 @@
+import os.path
+import unittest
+# from unittest.mock import patch  # имитация действия функции sys запуск из терминала
+import sys
+import server
+
+sys.path.append(os.path.join(os.getcwd(), '..'))
+print(sys.path)
+
+
+class TestProcessClientMessage(unittest.TestCase):
+
+    def test_structure_message(self):
+        message = {'a': 1, 'b': 2}
+        result = {'response': 400, 'error': 'Bad request'}
+        self.assertEqual(result, server.process_client_message(message))
