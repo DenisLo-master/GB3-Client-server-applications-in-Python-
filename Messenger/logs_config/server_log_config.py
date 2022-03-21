f8be0966c5b1a91d4464aa08f0197db0462aaf88
Denis <Denis.lkg@gmail.com>
2022-03-21 20:10:05 +0300

    hw4 unit-test finished

--- Messenger/logs_config/server_log_config.py
+++ Messenger/logs_config/server_log_config.py
@@ -4,30 +4,37 @@ import logging
 import os.path
 import sys
 from logging import handlers
-
 sys.path.append('../')
 from common.variables import LOGGING_REGISTRAR_LEVEL, STREAM_LOGGING_LEVEL, FILE_LOGGING_LEVEL
+
 
 # формировщик логов (dormatter)
 SERVER_FORMATTER = logging.Formatter('%(asctime)-26s %(levelname)-10s %(filename)-23s %(message)s')
 
 # подготовка файла для сбора логов
+print('__file__-', __file__)
 PATH = os.path.dirname(os.path.abspath(__file__))
-if not os.path.exists("logs"):
-    os.mkdir("logs")
-PATH = os.path.join(PATH, 'logs')
-PATH = os.path.join(PATH, 'server.log')
+print('print(PATH)-', PATH)
+MODULE_NAME = (str(PATH).split('/')[-1:][0])
+print('MODULE_NAME-', MODULE_NAME)
+LOG_DIR_NAME = MODULE_NAME + '_logs'
+print('LOG_DIR_NAME-', LOG_DIR_NAME)
+FILE_NAME = MODULE_NAME + '.log'
+print('FILE_NAME-', FILE_NAME)
+if not os.path.exists(LOG_DIR_NAME):
+    os.mkdir(LOG_DIR_NAME)
 
 
+PATH = os.path.join(PATH, LOG_DIR_NAME)
+PATH = os.path.join(PATH, FILE_NAME)
+print(PATH)
 
 #потоки вывода логов
 STREAM_HANDLER = logging.StreamHandler(sys.stderr)
 STREAM_HANDLER.setFormatter(SERVER_FORMATTER)
 STREAM_HANDLER.setLevel(STREAM_LOGGING_LEVEL)
 
-
-
-LOG_FILE = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf-8', interval=1, when='d')
+LOG_FILE = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf-8', interval=1, when='s')
 LOG_FILE.setFormatter(SERVER_FORMATTER)
 LOG_FILE.setLevel(FILE_LOGGING_LEVEL)
 
--- Messenger/server.py
+++ Messenger/server.py
@@ -11,7 +11,10 @@ from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from common.variables import ACTION, ACCOUNT_NAME, RESPONSE, MAX_CONNECTION, PRESENCE, TIME, USER, ERROR
 from common.utilites import get_message, send_message, check_port, check_address, validation_address_ipv4
 import json
+import logging
+import logs_config.server_log_config
 
+LOGGER = logging.getLogger('server')
 
 def process_client_message(message):
     """
@@ -21,15 +24,18 @@ def process_client_message(message):
     :param message:
     :return:
     """
-
+    LOGGER.debug(f'Разбор сообщения от клиента {message}')
     if isinstance(message, dict):
         if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
                 and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+            LOGGER.debug(f'Код:200, сообщение от клиента - {message}')
             return {RESPONSE: 200}
+        LOGGER.error(f'Код:400, структура сообщения не корректно - {message}')
         return {
             RESPONSE: 400,
             ERROR: 'Bad request'
         }
+    LOGGER.error(f'Код:400, не корректный тип сообщения - {message}')
     return {
         RESPONSE: 400,
         ERROR: 'Bad request'
@@ -37,44 +43,56 @@ def process_client_message(message):
 
 
 def main():
-    print('запуск сервера')
+    LOGGER.info(f'запуск сервера')
     try:
         listen_port = check_port()
+        LOGGER.debug(f'порт сервера определен')
     except IndexError:
+        LOGGER.error('После параметра -\'p\' необходимо указать номер порта для подключения')
         sys.exit('После параметра -\'p\' необходимо указать номер порта для подключения')
     except ValueError:
+        LOGGER.error('В качестве порта укажите значение от 1024 до 65535')
         sys.exit('В качестве порта укажите значение от 1024 до 65535')
 
     try:
         listen_address = validation_address_ipv4(check_address())
+        LOGGER.debug(f'адрес сервера определен')
     except IndexError:
+        LOGGER.error('не указан IP сервераэ, после -a')
         sys.exit('После параметра -\'a\' можно указать IP адрес сервера')
     except TypeError:
+        LOGGER.error('IP адрес указан не правильно')
         sys.exit('IP адрес указан не правильно, запишите в формате 0.0.0.0')
     except ValueError:
+        LOGGER.error('указан некорректный IP адрес сервера')
         sys.exit('указан некорректный IP адрес сервера')
 
     # готовим сокет
+    LOGGER.debug(f'запуск сокета')
     transport = socket(AF_INET, SOCK_STREAM)
+    LOGGER.debug(f'установка параметров сокета')
     transport.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     transport.bind((listen_address, listen_port))
 
     # слушаем порт на входящие подключения
     transport.listen(MAX_CONNECTION)
-    print('сервер запущен, ожидает подклчение клиентов')
+    LOGGER.info(f'сервер {listen_address}:{listen_port} запущен, ожидает подклчение клиентов')
 
     while True:
         client_socket, client_address = transport.accept()
-        print(f'подключение клиента {client_address[0]}:{client_address[1]}')
+        LOGGER.info(f'подключение клиента {client_address[0]}:{client_address[1]}')
         try:
             message_from_client = get_message(client_socket)
+            LOGGER.info(f'Получено сообщение от {client_address[0]}')
             response = process_client_message(message_from_client)
             send_message(client_socket, response)
+            LOGGER.info(f'Cообщение для {client_address[0]} отправлено')
             client_socket.close()
-            print(f'Клиент подключен {client_address[0]}:{client_address[1]}')
+            LOGGER.info(f'Сокет закрыт {client_address[0]}:{client_address[1]}')
         except (ValueError, json.JSONDecodeError):
-            print(f'Получено не корректное сообщение от клиента {client_address[0]}:{client_address[1]}')
+            LOGGER.error(f'Получено не корректное сообщение от клиента {client_address[0]}:{client_address[1]}')
             client_socket.close()
+            LOGGER.info(f'Сокет закрыт {client_address[0]}:{client_address[1]}')
 
 
 if __name__ == '__main__':
